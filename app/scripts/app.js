8b2e6ebdfc8909270aeb0a10baf322ac99b917fc
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-09-25 22:08:36 +0300

    I made a dextop animation of cards and json

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
-import svg4everybody from 'svg4everybody';
+import DATA from "./data";
 import $ from 'jquery';
 
+const { img } = DATA;
+
 $(() => {
-	svg4everybody();
+	$.each(img, function (index, img) {
+		let clone = $('#card').clone();
+		clone.find('img').attr('src', img.imageSrc);
+		clone.appendTo('.cards__content');
+		clone.removeAttr('id');
+	});
+
+	$(document).ready(function () {
+		$('.cards__card').click(function () {
+		  $(this).toggleClass('flipped');
+		});
+	});
 });
---
+++ app/scripts/data.js
@@ -0,0 +1,14 @@
+const DATA = {
+    img:[
+        {
+            imageSrc: "./images/cart_2.png"
+
+        },
+        {
+            imageSrc: "./images/cart_3.png"
+
+        },
+    ],
+};
+
+export default DATA;
