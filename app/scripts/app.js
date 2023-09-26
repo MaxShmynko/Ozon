3ba8295a3ee07323a8c3aeda358be01354457546
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-09-26 17:11:12 +0300

    I made the transfer of text and images through js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,10 @@ const { img } = DATA;
 $(() => {
 	$.each(img, function (index, img) {
 		let clone = $('#card').clone();
-		clone.find('img').attr('src', img.imageSrc);
+		clone.find('img:eq(0)').attr('src', img.imageSrc1);
+		clone.find('img:eq(1)').attr('src', img.imageSrc2);
+		clone.find('.cards__face-title').text(img.text1);
+		clone.find('.cards__face-text').text(img.text2);
 		clone.appendTo('.cards__content');
 		clone.removeAttr('id');
 	});
