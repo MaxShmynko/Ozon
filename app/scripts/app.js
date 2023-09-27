4d8e6ee858e434f3c692a9ece03c78e1782e1f2b
shmunko.maks99 <shmunko.maks99@gmail.com>
2023-09-27 21:31:35 +0300

    changes in json and app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,27 +4,79 @@ import $ from 'jquery';
 const { itemData } = DATA;
 
 $(() => {
-	$.each(itemData, function (index, itemData) {
-		let clone = $('#card').clone();
-		clone.find('img:eq(0)').attr('src', itemData.imageSrc1);
-		clone.find('img:eq(1)').attr('src', itemData.imageSrc2);
-		clone.find('.cards__face-title').text(itemData.text1);
-		clone.find('.bold-text').text(itemData.boldText);
-		clone.find('.normal-text1').text(itemData.text2);
-		clone.find('.face-link-bank').text(itemData.linkText1).attr('href', itemData.linkURL1);
-		clone.find('.normal-text2').text(itemData.text3);
-		clone.find('.face-link-stocks').text(itemData.linkText2).attr('href', itemData.linkURL2);
-		clone.find('.normal-text3').text(itemData.text4);
-		clone.appendTo('.cards__content');
-		clone.removeAttr('id');
-	  });
 
-	$(document).ready(function () {
+	function getRandomIndexes(array, count) {
+		let indexes = [];
+		let currentIndex = array.length;
+		let temporaryValue, randomIndex;
+	
+		let newArray = array.slice();
+	
+		while (currentIndex > 0 && indexes.length < count) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+	
+			temporaryValue = newArray[currentIndex];
+			newArray[currentIndex] = newArray[randomIndex];
+			newArray[randomIndex] = temporaryValue;
+	
+			indexes.push(randomIndex);
+		}
+	
+		return indexes;
+	}
+	
+	function updateContent() {
+		let randomIndexes;
+	
+		
+		if ($(window).width() <= 800) {
+			randomIndexes = getRandomIndexes(itemData, 1);
+		} else {
+			randomIndexes = getRandomIndexes(itemData, 3);
+		}
+	
+		let desktopRandomElements = randomIndexes.map(function (index) {
+			return itemData[index];
+		});
+	
+		let content = $('.cards__content');
+	
+		content.empty();
+	
+		$.each(desktopRandomElements, function (index, itemData) {
+			let $block = $('<div class="cards__card"></div>');
+			let $image1 = $('<div class="cards__face card__face-front"><div class="cards__img"><img src="' + itemData.imageSrc1 + '" alt="Изображение 1"></div></div>');
+			let $image2 = $('<div class="cards__face card__face-back"><div class="cards__img back-img"><img src="' + itemData.imageSrc2 + '" alt="Изображение 2"></div><div class="cards__face-title">' + itemData.text1 + '</div><div class="cards__face-text"> <span class="bold-text">' + itemData.boldText + '</span><span class="normal-text1">' + itemData.text2 + '</span><a class="face-link-bank" href="' + itemData.linkURL1 + '">' + itemData.linkText1 + '</a><span class="normal-text3">' + itemData.text3 + '</span><a class="face-link-bank" href="' + itemData.linkURL2 + '">' + itemData.linkText2 + '</a><span class="normal-text4">' + itemData.text4 + '</span></div></div>');
+	
+			$block.append($image1, $image2);
+	
+			content.append($block);
+		});
+	
+		assignEventHandlers();
+	}
+	
+	function assignEventHandlers() {
 		$('.cards__face-text').click(function (event) {
 			event.stopPropagation();
 		});
+	
 		$('.cards__card').click(function () {
-		  $(this).toggleClass('flipped');
+			$(this).toggleClass('flipped');
+		});
+	}
+	
+	$(document).ready(function () {
+		updateContent(itemData);
+	
+		$('.cards__restart').click(function () {
+			updateContent();
+		});
+	
+		$(window).resize(function () {
+			updateContent();
 		});
 	});
+
 });
